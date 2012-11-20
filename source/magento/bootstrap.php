acdb6392369305bbe61117ec030b5573e1005c82
Carsten Schipke <carsten.schipke@gmail.com>
2012-11-20 12:40:45 +0800

    * [common] Integrated demo stores to test multi-tenan4cy related features/modifications. * [security] Use Admin_Model_Roles instead of Admin_Model_Role (seem to be refactored). * [design/frontend/ultimo] Support fallback/overwriting of static resources to default store/by store view.

---
+++ source/magento/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+
+
+  function tmo_magento_bootstrap()
+  {
+    if(@is_file(MAGENTO_ROOT.'/app/etc/local.php'))
+      require_once MAGENTO_ROOT.'/app/etc/local.php';
+
+    session_start();
+
+    if(false===strpos($_SERVER['REQUEST_URI'], '/admin'))
+    {
+      Mage::init();
+
+      $model=Mage::getResourceModel('core/website');
+      $stmt=$model->getReadConnection()->query(sprintf(
+        'SELECT code FROM %1$s WHERE name = \'%2$s\'',
+          $model->getMainTable(),
+          strtolower(trim($_SERVER['HTTP_HOST']))
+      ));
+
+      if(!$website=$stmt->fetch(PDO::FETCH_COLUMN, 0))
+      {
+        $stmt=$model->getReadConnection()->query(sprintf(
+          'SELECT code FROM %1$s WHERE is_default = 1',
+            $model->getMainTable()
+        ));
+
+        $website=$stmt->fetch(PDO::FETCH_COLUMN, 0);
+      }
+
+      Mage::run($website, 'website');
+    }
+    else
+    {
+      Mage::run('admin', 'store');
+    }
+  }
+?>
